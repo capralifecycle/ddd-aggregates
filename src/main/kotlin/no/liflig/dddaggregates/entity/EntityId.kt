bba432296f130eda3486e7e43fe6a3f6ec4d6380
Henrik Steen <henrist@henrist.net>
2020-12-11 18:46:32 +0100

    Richer mapping utils

--- src/main/kotlin/no/liflig/dddaggregates/entity/EntityId.kt
+++ src/main/kotlin/no/liflig/dddaggregates/entity/EntityId.kt
@@ -1,6 +1,7 @@
 package no.liflig.dddaggregates.entity
 
 import arrow.core.Either
+import arrow.core.flatMap
 import arrow.core.left
 import arrow.core.right
 import kotlinx.serialization.KSerializer
@@ -14,7 +15,6 @@ import org.jdbi.v3.core.argument.Argument
 import org.jdbi.v3.core.config.ConfigRegistry
 import java.sql.Types
 import java.util.UUID
-import kotlin.reflect.KFunction1
 
 /**
  * EntityId represents the ID pointing to a specific [Entity].
@@ -31,30 +31,72 @@ interface UuidEntityId : EntityId {
 typealias StringMapper<T> = (String) -> Either<IllegalArgumentException, T>
 
 /**
- * Create a mapper function to convert a [String] holding an UUID into a known [T].
+ * Evaluate the block and map an [IllegalArgumentException] into left side of [Either].
  */
-fun <T> createUuidMapper(factory: KFunction1<UUID, T>): StringMapper<T> =
-  { it: String ->
-    try {
-      factory(UUID.fromString(it)).right()
-    } catch (e: IllegalArgumentException) {
-      e.left()
+inline fun <T> catchArgument(block: () -> T): Either<IllegalArgumentException, T> =
+  try {
+    block().right()
+  } catch (e: IllegalArgumentException) {
+    e.left()
+  }
+
+/**
+ * Wrap a function so any [IllegalArgumentException] thrown will be returned into left side of [Either].
+ */
+inline fun <T, R> handleIllegalArgument(crossinline block: (T) -> R): (T) -> Either<IllegalArgumentException, R> =
+  {
+    catchArgument {
+      block(it)
     }
   }
 
 /**
- * Create a pair representing the mapping of a specific [T] from an UUID stored
- * in a [String] by using the provided [factory] function.
+ * Parse a [String] into [UUID] with error handling.
  */
+fun parseUuid(value: String): Either<IllegalArgumentException, UUID> =
+  catchArgument {
+    UUID.fromString(value)
+  }
+
+/**
+ * Create a mapper function to convert a [String] holding an [UUID] into a known [T].
+ *
+ * [IllegalArgumentException] in the conversion will be handled.
+ */
+fun <T> createUuidMapper(factory: (UUID) -> T): StringMapper<T> =
+  {
+    parseUuid(it).flatMap(handleIllegalArgument(factory))
+  }
+
+/**
+ * Create a pair representing the mapping of a specific [T] from an [UUID] stored
+ * in a [String] by using the provided [factory] function.
+ *
+ * [IllegalArgumentException] in the conversion will be handled.
+ */
+@JvmName("createMapperPairForUuid")
 inline fun <reified T> createMapperPair(
-  factory: KFunction1<UUID, T>
+  noinline factory: (UUID) -> T
 ): Pair<Class<T>, StringMapper<T>> =
   T::class.java to createUuidMapper(factory)
 
 /**
  * Create a pair representing the mapping of a specific [T] from a [String]
  * by using the provided [factory] function.
+ *
+ * [IllegalArgumentException] in the conversion will be handled.
  */
+@JvmName("createMapperPairForString")
+inline fun <reified T> createMapperPair(
+  noinline factory: (String) -> T
+): Pair<Class<T>, StringMapper<T>> =
+  T::class.java to handleIllegalArgument(factory)
+
+/**
+ * Create a pair representing the mapping of a specific [T] from a [String]
+ * by using the provided [factory] function.
+ */
+@JvmName("createMapperPairForStringMapper")
 inline fun <reified T> createMapperPair(
   noinline factory: StringMapper<T>
 ): Pair<Class<T>, StringMapper<T>> =
